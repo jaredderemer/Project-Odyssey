84c78cc55dac813390e424e98d972e503e19fa81
caseward <casey.fenton@outlook.com>
2017-02-03 00:09:14 -0600

    Player Controller
    Controls most of the basic functions of the character.

---
+++ Assets/playerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// Movement variables
+	public float runSpeed;
+
+	Rigidbody myRB;
+	Animator  myAnim;
+
+    bool facingRight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRB   = GetComponent<Rigidbody>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight          = !facingRight;
+        Vector3 zedScale     = transform.localScale;
+        zedScale.z          *= -1;
+        transform.localScale = zedScale;
+    }
+}

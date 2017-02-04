c51eb50d7df0edc392ad6306e9313c396af10a32
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-03 21:38:36 -0600

    Scrolling credits working

---
+++ Assets/scrollingText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class scrollingText : MonoBehaviour {
+
+	Text scrollText;
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		scrollText = GameObject.Find("scrollText").GetComponent<Text>();
+		speed = .3f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = scrollText.transform.position;
+		position.y += speed;
+		scrollText.transform.position = position;
+	}
+}

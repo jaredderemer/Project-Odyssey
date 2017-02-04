bdf4960cba0351640f6a78e5a0885dc3d795da60
Juju Moong <Juju@juju-air.local>
2017-02-04 16:20:13 -0600

    Updated scene 1

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+   private float min;
+   private float max;
+   public float length;
+   public float speed;
+   private GameObject target;
+   private Vector3 offset;
+
+   // Use this for initialization
+   void Start () {
+      min = transform.position.x;
+      max = transform.position.x + length;
+      target = null;
+   }
+
+   void OnTriggerStay(Collider thing)
+   {
+      target = thing.gameObject;
+      offset = target.transform.position - transform.position;
+   }
+   void OnTriggerExit(Collider thing)
+   {
+      target = null;
+   }
+   // Update is called once per frame
+   void Update () {
+      transform.position =new Vector3(Mathf.PingPong(Time.time*speed,max-min)+min, 
+                                      transform.position.y, 
+                                      transform.position.z);
+
+      if (target != null) 
+      {
+         target.transform.position = transform.position + offset;
+      }
+   }
+}

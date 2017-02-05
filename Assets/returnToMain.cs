26cf7c532040d2e0fe2ab2098e230bb25df0b09c
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-04 21:55:23 -0600

    Working on return to main menu from pause panel

---
+++ Assets/returnToMain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class returnToMain : MonoBehaviour
+{
+	//private Button mainMenu;
+	GameObject UI;
+
+	// Use this for initialization
+	void Start () {
+		UI = GameObject.Find("UI");
+	}
+	
+	// Update is called once per frame
+	public void ReturnToMain () {
+		SceneManager.LoadScene ("mainMenu");
+		Destroy (UI);
+	}
+}

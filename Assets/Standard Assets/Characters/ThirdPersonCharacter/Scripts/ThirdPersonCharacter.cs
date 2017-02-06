6db18b4fbf7c2a3176a63ab77af35e5841d10e70
NickTheRedFox <NickTheRedFox@gmail.com>
2017-02-06 16:57:15 -0600

    Object: Added Banana animation (Rotates) + some scripts to disable it's gravity

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -10,7 +10,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 	{
 		[SerializeField] float m_MovingTurnSpeed = 360;
 		[SerializeField] float m_StationaryTurnSpeed = 180;
-		[SerializeField] float m_JumpPower = 12f;
+		[SerializeField] float m_JumpPower = 18f;
 		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
 		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
 		[SerializeField] float m_MoveSpeedMultiplier = 1f;
@@ -206,7 +206,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         {
             if (other.gameObject.CompareTag("Pickups"))
             {
-                other.gameObject.SetActive(false);
+                //other.gameObject.SetActive(false);
+				other.GetComponent<Rigidbody>().useGravity = false;
             }
         }
 

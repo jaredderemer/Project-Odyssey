f33f83de89bbb41ce66c440611e5de6a35a85f1d
caseward <casey.fenton@outlook.com>
2017-02-14 12:08:19 -0600

    sprintSpeed
    NOW sprint is properly reflected

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -2,85 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class playerController : MonoBehaviour
+{
 
-	// Movement variables
-	public float runSpeed;
-    public float walkSpeed;
+   // Movement variables
+   public float runSpeed;
+   public float sprintSpeed;
 
-	Rigidbody myRB;
-	Animator  myAnim;
+   Rigidbody myRB;
+   Animator myAnim;
 
-    bool facingRight;
+   bool facingRight;
 
-    // for jumping
-    // Character is starting slightly off the ground, otherwise change to TRUE
-    bool grounded = false;
-    Collider[] groundCollisions;
-    float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
-    public Transform groundCheck;
-    public float jumpHeight;
+   // for jumping
+   // Character is starting slightly off the ground, otherwise change to TRUE
+   bool grounded = false;
+   Collider[] groundCollisions;
+   float groundCheckRadius = 0.2f;
+   public LayerMask groundLayer;
+   public Transform groundCheck;
+   public float jumpHeight;
 
-	// Use this for initialization
-	void Start ()
-    {
-        myRB   = GetComponent<Rigidbody>();
-        myAnim = GetComponent<Animator>();
-        facingRight = true;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+   // Use this for initialization
+   void Start()
+   {
+      myRB = GetComponent<Rigidbody>();
+      myAnim = GetComponent<Animator>();
+      facingRight = true;
+   }
 
-    private void FixedUpdate()
-    {
-        if (grounded && Input.GetAxis("Jump") > 0)
-        {
-            grounded = false;
-            myAnim.SetBool("grounded", grounded);
-            myRB.AddForce(new Vector3(0, jumpHeight, 0));
-        }
-        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+   // Update is called once per frame
+   void Update()
+   {
 
-        if (groundCollisions.Length > 0)
-            grounded = true;
-        else
-            grounded = false;
+   }
 
-        myAnim.SetBool("grounded", grounded);
+   private void FixedUpdate()
+   {
+      if (grounded && Input.GetAxis("Jump") > 0)
+      {
+         grounded = false;
+         myAnim.SetBool("grounded", grounded);
+         myRB.AddForce(new Vector3(0, jumpHeight, 0));
+      }
+      groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
 
-        float move = Input.GetAxis("Horizontal");
-        myAnim.SetFloat("speed", Mathf.Abs(move));
+      if (groundCollisions.Length > 0)
+         grounded = true;
+      else
+         grounded = false;
 
-        myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+      myAnim.SetBool("grounded", grounded);
 
-        float sprinting = Input.GetAxisRaw("Fire3");
-        myAnim.SetFloat("sprinting", sprinting);
+      float move = Input.GetAxis("Horizontal");
+      myAnim.SetFloat("speed", Mathf.Abs(move));
 
-        if(sprinting > 0 && grounded)
-        {
-            myRB.velocity = new Vector3(move * walkSpeed, myRB.velocity.y, 0);
-        }
-        else
-        {
-            myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
-        }
+      myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
 
-        if (move > 0 && !facingRight)
-            Flip();
-        else if (move < 0 && facingRight)
-            Flip();
-    }
+      float sprinting = Input.GetAxisRaw("Fire3");
+      myAnim.SetFloat("sprinting", sprinting);
 
-    void Flip()
-    {
-        facingRight          = !facingRight;
-        Vector3 zedScale     = transform.localScale;
-        zedScale.z          *= -1;
-        transform.localScale = zedScale;
-    }
+      if (sprinting > 0 && grounded)
+      {
+         myRB.velocity = new Vector3(move * sprintSpeed, myRB.velocity.y, 0);
+      }
+      else
+      {
+         myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+      }
+
+      if (move > 0 && !facingRight)
+         Flip();
+      else if (move < 0 && facingRight)
+         Flip();
+   }
+
+   void Flip()
+   {
+      facingRight = !facingRight;
+      Vector3 zedScale = transform.localScale;
+      zedScale.z *= -1;
+      transform.localScale = zedScale;
+   }
 }

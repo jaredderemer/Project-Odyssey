7e7b71db3b98d6af867e4e6c856a6784c2c0ef63
Juju Moong <Juju@juju-air.local>
2017-02-19 21:52:34 -0600

    scene 1 background (draft 2) added; still working on scene 2

--- Assets/Scripts/DestroyItem.cs
+++ Assets/Scripts/DestroyItem.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class DestroyItem : MonoBehaviour {
 
-
    void OnTriggerStay(Collider collider)
    {
       if (Input.GetKey (KeyCode.E)) 
       {
-         if (collider.gameObject.tag == "Player") 
+         if (collider.gameObject.tag == "Weapon") 
          {
             Destroy (gameObject);
          }
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+   //public int flashLength;
+   Renderer rend;
+	// Use this for initialization
+	void Start () {
+      rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      FlashObject ();
+	}
+
+   IEnumerator FlashObject()
+   {
+      //yield return new WaitForSeconds (100);
+      //for (int i = 1; i <= flashLength; i++) 
+      //{
+      rend.enabled = false;
+         yield return new WaitForSeconds (10);
+         rend.enabled = true;
+         yield return new WaitForSeconds (10);
+     // }
+   }
+}

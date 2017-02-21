3226be29a0179acb324448e05511085d63628033
Jonathan Rigsby <124256@studentnet.int>
2017-02-21 15:26:37 -0600

    Added decision hut scene, camera lookat and climbing scripts

---
+++ Assets/Scripts/CameraFollowHut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowHut : MonoBehaviour
+{
+   public Transform target;
+    
+   void LateUpdate ()
+   {
+      if (target.position.x < -4.9f && target.position.x > -12.9f)
+      {
+         transform.LookAt (target.transform);
+      }
+   }
+}
---
+++ Assets/Scripts/ClimbLadder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbLadder : MonoBehaviour
+{
+   public Transform CameraView;
+    
+   void LateUpdate ()
+   {
+      // While the player is positioned in front of the ladder and pressing W
+      if (transform.position.x < -16.5f && transform.position.x > -17.5f &&
+          transform.position.y > -1f && transform.position.y < 12f && Input.GetKey("q"))
+      {
+         transform.Translate(Vector3.up * 0.1f, Space.World);
+         Debug.Log("Climbing");
+      }
+      else if (transform.position.x < -16f && transform.position.x > -18f)
+      {
+         // Display "W" key prompt
+      }
+   }
+}

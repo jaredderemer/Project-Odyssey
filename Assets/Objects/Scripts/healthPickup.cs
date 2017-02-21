43d623522c150625c93c1d7aee08e5391b3d3d6e
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-20 22:00:22 -0600

    Integrating bananas into scene1

---
+++ Assets/Objects/Scripts/healthPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthPickup : MonoBehaviour {
+
+	public int healthAmount,
+               timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+       
+	}
+	
+	// Update is called once per frame
+    void Update() 
+	{   
+       // Auto increments item timer and destroys it if it isnt picked up
+       
+      
+
+	}
+	
+	// Activates upon contact with player
+    // Will call player's add health script and heal them
+	void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<playerHealth>().addHealth(healthAmount);
+            Destroy(gameObject);
+        }
+	}
+}

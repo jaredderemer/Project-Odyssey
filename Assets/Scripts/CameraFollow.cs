cd6e73fc741c773d87dba8e3f48a94d933658266
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-21 16:57:29 -0600

    Merging in character into Environment branch, changed camera follow to Casey's

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,45 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour 
+public class CameraFollow : MonoBehaviour
 {
-   public Transform target;
+    public Transform target; // What the camera is going to follow
+    public float smoothing = 7.5f; // 
+    
+    public float edgeLeft;
+    public float edgeRight;
+    
+    float halfWidth;
+    float maxPosition;
+    float minPosition;
 
-   public float smoothing = 5f;
-   public float edgeLeft;
-   public float edgeRight;
+    Vector3 offset; // Distance from target camera maintains
+    Vector3 targetCamPos;
 
-   float halfWidth;
-   float maxPosition;
-   float minPosition;
+    // Use this for initialization
+    void Start ()
+    {
+        offset      = transform.position - target.position;
+        halfWidth   = Camera.main.orthographicSize * Screen.width / Screen.height;
+        maxPosition = edgeRight - halfWidth;
+        minPosition = edgeLeft - halfWidth;
+    }
+    
+    void FixedUpdate()
+    {
+       if (target.position.x >= maxPosition) 
+        {
+            targetCamPos = new Vector3 (maxPosition, transform.position.y, transform.position.z);
+        } 
+        else if (target.position.x <= minPosition) 
+        {
+            targetCamPos = new Vector3 (minPosition, transform.position.y, transform.position.z);
+        }
+        else 
+        {
+            targetCamPos = target.position + offset;
+        }
 
-   Vector3 offset;
-   Vector3 targetCamPos;
-
-	// Use this for initialization
-	void Start () 
-   {
-      offset      = transform.position - target.position;
-      halfWidth   = Camera.main.orthographicSize * Screen.width / Screen.height;
-      maxPosition = edgeRight - halfWidth;
-      minPosition = edgeLeft - halfWidth;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-   {
-      if (target.position.x >= maxPosition) 
-      {
-         targetCamPos = new Vector3 (maxPosition, transform.position.y, transform.position.z);
-      } 
-      else if (target.position.x <= minPosition) 
-      {
-         targetCamPos = new Vector3 (minPosition, transform.position.y, transform.position.z);
-      }
-      else 
-      {
-         targetCamPos = target.position + offset;
-      }
-      transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
-   }
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
 }

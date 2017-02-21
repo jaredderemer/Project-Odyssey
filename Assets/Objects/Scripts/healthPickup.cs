2520e340dbef70f5084953f4daff69fe5691194e
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-21 13:22:46 -0600

    Made small formatting changes to healthPickup.cs

--- Assets/Objects/Scripts/healthPickup.cs
+++ Assets/Objects/Scripts/healthPickup.cs
@@ -1,33 +1,28 @@
-﻿using System.Collections;
+﻿/********************************************************************** 
+* Author            MM/DD/YY HH24:MM   Description                    * 
+* Nicholas Oliver   02/05/17 23:05     Created script to handle adding*
+*									   health to player when colliding* 
+*									   with banana                    * 
+*									   								  *
+* Jared DeRemer     02/21/17 11:40     Made some small formatting 	  *
+*									   changes      				  * 
+**********************************************************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class healthPickup : MonoBehaviour {
 
-	public int healthAmount,
-               timer;
+	public int healthAmount;
 
-	// Use this for initialization
-	void Start () 
-	{
-       
-	}
-	
-	// Update is called once per frame
-    void Update() 
-	{   
-       // Auto increments item timer and destroys it if it isnt picked up
-       
-      
-
-	}
-	
-	// Activates upon contact with player
-    // Will call player's add health script and heal them
+	// Triggers when an object collides with a banana object
 	void OnTriggerEnter(Collider other)
 	{
+		// Check if event was triggered by the Player object
         if (other.gameObject.CompareTag("Player"))
         {
+			// Add health to Player and destroy the banana object
             other.GetComponent<playerHealth>().addHealth(healthAmount);
             Destroy(gameObject);
         }

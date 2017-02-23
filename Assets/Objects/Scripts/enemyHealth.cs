8a8038dca8b763d0b0e6810903cdde59359646c5
NickTheRedFox <NickTheRedFox@gmail.com>
2017-02-22 20:49:15 -0600

    Environment: Merged my object scripts and other objects into Environment

---
+++ Assets/Objects/Scripts/enemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour 
+{
+    public int health;  // Enemy health
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    public void addDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}

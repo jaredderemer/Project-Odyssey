46606d0f6e959df5dcceee157736a78662a00803
Jared DeRemer <jaredderemer@yahoo.com>
2017-02-22 21:54:40 -0600

    Merging in some environment and character stuff into Menus branch for testing health gui.

---
+++ Assets/Environment/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // What the camera is going to follow
+    public float smoothing = 7.5f; // 
+    
+    public float edgeLeft;
+    public float edgeRight;
+    
+    float halfWidth;
+    float maxPosition;
+    float minPosition;
+
+    Vector3 offset; // Distance from target camera maintains
+    Vector3 targetCamPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset      = transform.position - target.position;
+        halfWidth   = Camera.main.orthographicSize * Screen.width / Screen.height;
+        maxPosition = edgeRight - halfWidth;
+        minPosition = edgeLeft - halfWidth;
+    }
+    
+    void FixedUpdate()
+    {
+       if (target.position.x >= maxPosition) 
+        {
+            targetCamPos = new Vector3 (maxPosition, transform.position.y, transform.position.z);
+        } 
+        else if (target.position.x <= minPosition) 
+        {
+            targetCamPos = new Vector3 (minPosition, transform.position.y, transform.position.z);
+        }
+        else 
+        {
+            targetCamPos = target.position + offset;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}

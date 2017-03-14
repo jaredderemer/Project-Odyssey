7e5edabfed6ba9839f37686c6bd843f7d1301b38
Jared DeRemer <jaredderemer@yahoo.com>
2017-03-14 16:46:15 -0500

    Working on scene2 changes, Interact prompts finished

--- Assets/promptInteract.cs
+++ Assets/promptInteract.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class promptInteract : MonoBehaviour
 {
-   private GameObject interactPrompt; // The prompt gameObject
-   public Transform interactable;     // The interactable object
-   private bool interacted = false;   // State of object interaction
-   private Vector3 newPosition;       // The new position of the prompt
-   public float yPosition; 			  // The height above the object for the prompt
+   private GameObject interactPrompt;     // The prompt gameObject
+   public GameObject promptTextObject;   // The prompt text gameObject
+   private GameObject promptPanel;
+   public string     text;               // Text to show on prompt
+   
+   public  Transform interactable;       // The interactable object
+   private bool      interacted = false; // State of object interaction
+   private Vector3   newPosition;        // The new position of the prompt
+   public  float     yPosition; 			  // The height above the object for the prompt
 
 	// Use this for initialization
 	void Start ()
    {
+      promptPanel = GameObject.Find("PromptPanel");
+      
+      
       interactPrompt = GameObject.Find("InteractPrompt");
-      interactPrompt.SetActive(true);
+      
+      //promptPanel = interactPrompt.transform.GetChild(0).gameObject;
 	}
    
    void OnTriggerEnter(Collider other)
@@ -22,31 +31,42 @@ public class promptInteract : MonoBehaviour
       // Check if the object has been interacted with
       if (!interacted && other.gameObject.CompareTag("Player"))
       {
+         // Set prompt text
+         promptTextObject.GetComponent<Text>().text = text;
+         
          // Reposition the prompt above this object
          newPosition = interactable.position;
          newPosition.y += yPosition;
          Debug.Log(newPosition);
          interactPrompt.transform.position = newPosition;
          
-         // Ativate the prompt
-         interactPrompt.SetActive(true);
+         // Activate the prompt
+         //interactPrompt.SetActive(true);
+         promptPanel.SetActive(true);
       }
    }
    
+   // Display the prompt if the player is near the object
    void OnTriggerStay(Collider other)
    {
+      // Check if the player interacts
       if (Input.GetKey(KeyCode.E) && other.tag == "Player")
       {
-         interactPrompt.SetActive(false);
+         // Hide the prompt after the player interacts
+         //interactPrompt.SetActive(false);
+         promptPanel.SetActive(false);
+         
          interacted = true;
       }
    }
    
+   // Hide the prompt after the player moves away from the object
    void OnTriggerExit(Collider other)
    {
-      if (other.gameObject.CompareTag("Player"))
+      if (!interacted && other.gameObject.CompareTag("Player"))
       {
-         interactPrompt.SetActive(false);
+         //interactPrompt.SetActive(false);
+         promptPanel.SetActive(false);
       }
    }
 }

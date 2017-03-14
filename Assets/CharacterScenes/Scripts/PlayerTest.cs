63bb097307841b4f062e17c2a72586599fb1f7c8
caseward <casey.fenton@outlook.com>
2017-03-14 11:51:49 -0500

    nothing to see here, move along

---
+++ Assets/CharacterScenes/Scripts/PlayerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+   public float   speed       = 150f;
+   public Vector3 maxVelocity = new Vector3(60, 100, 0);
+
+   private Rigidbody myBody;
+   private Animator  myAnimator;
+
+   // Use this for initialization
+   void Start()
+   {
+      myBody     = GetComponent<Rigidbody>();
+      myAnimator = GetComponent<Animator> ();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      var absVelX = Mathf.Abs(myBody.velocity.x);
+
+      var forceX = 0f;
+      var forceY = 0f;
+
+      if (Input.GetKey("right"))
+      {
+         if(absVelX < maxVelocity.x)
+         {
+            forceX = speed;
+         }
+         else if (Input.GetKey("left"))
+         {
+            if(absVelX <maxVelocity.x)
+            {
+               forceX = -speed;
+            }
+         }
+
+         myBody.AddForce(new Vector3(forceX, forceY, 0));
+      }
+   }
+}

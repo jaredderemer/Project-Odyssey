2948e8b6aae0c832dd385f57660db4c4e33bdcd1
caseward <casey.fenton@outlook.com>
2017-03-20 20:48:45 -0500

    Change comes to us all

--- Assets/CharacterScenes/Scripts/playerController.cs
+++ Assets/CharacterScenes/Scripts/playerController.cs
@@ -5,45 +5,49 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 // Public variables
-   // movingment variables
-   public float      runSpeed;
-   public float      walkSpeed;
+   // Movement variables
+   public float       runSpeed;
+   public float       walkSpeed;
 
    // Jumping variables
-   public LayerMask  groundLayer;
-   public Transform  groundCheck;
-   public float      jumpHeight;
+   public LayerMask   groundLayer;
+   public Transform   groundCheck;
+   public float       jumpHeight;
 
 // Private Variables
    // Character Control
-   private Rigidbody myRig;
-   private Animator  myAnim;
+   private Rigidbody  myRig;
+   private Animator   myAnim;
 
    // Character Direction
-   private bool      facingRight;
+   private bool       facingRight;
 
    // for jumping
    private bool       grounded = false; // Character is starting off the ground, otherwise change to TRUE
    private Collider[] groundCollisions;
-   private float      groundCheckRadius = 0.2f;
+   private float      groundCheckRadius = 0.2f; // Based off experience, this code should just work...
 
+// Class functions
    // Use this for initialization
    void Start()
    {
-      myRig       = GetComponent<Rigidbody>();
-      myAnim      = GetComponent<Animator> ();
-      facingRight = true;
+      myRig         = GetComponent<Rigidbody>();
+      myAnim        = GetComponent<Animator> ();
+      facingRight   = true; // Character starts facing right
+      myAnim.SetInteger("AnimState", 0);
    }
 
    // Using FixedUpdate() for physics' sake
    void FixedUpdate()
    {
+      // Small area under character to determine if character is on the ground or not
       groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
 
       if (grounded && Input.GetAxis("Jump") > 0)
       {
          grounded = false;
          myAnim.SetBool("grounded", grounded);
+         myAnim.SetInteger("AnimState", 3);
          myRig.AddForce(new Vector3(0, jumpHeight, 0));
       }
 
@@ -56,38 +60,50 @@ public class playerController : MonoBehaviour
       // Character is animated properly
       myAnim.SetBool("grounded", grounded);
 
-      float moving = Input.GetAxis("Horizontal"); // Get movingment input
-      myAnim.SetInteger("AnimState", 1); // Animate movingment
+      // Get movement input
+      float moving = Input.GetAxis("Horizontal");
 
-      myRig.velocity = new Vector3(moving * runSpeed, myRig.velocity.y, 0); // moving Character
+      // Move Character
+      myRig.velocity = new Vector3(moving * runSpeed, myRig.velocity.y, 0);
 
-      float walking = Input.GetAxisRaw("Fire3"); // Get walk Input
-      myAnim.SetInteger("AnimState", 2); // Animate walk
+      // Get walk Input
+      float walking = Input.GetAxisRaw("Fire3");
 
-      if (walking > 0)
+      if (moving != 0 && walking > 0)
       {
+         // Move character at walk speed
          myRig.velocity = new Vector3(moving * walkSpeed, myRig.velocity.y, 0);
+
+         // Animate walk
+         myAnim.SetInteger("AnimState", 2);
       }
-      else if (walking == 0)
+      else if (moving != 0 && walking == 0)
       {
+         // Move character at run speed
          myRig.velocity = new Vector3(moving * runSpeed, myRig.velocity.y, 0);
+
+         // Animate run
+         myAnim.SetInteger("AnimState", 1);
       }
       else
       {
+         // Animate idle
          myAnim.SetInteger("AnimState", 0);
       }
 
+      // Character facing correct direction?
       if (moving > 0 && !facingRight)
          Flip();
       else if (moving < 0 && facingRight)
          Flip();
    }
 
+   // Reverses the character's direction
    void Flip()
    {
-      facingRight = !facingRight;
-      Vector3 zedScale = transform.localScale;
-      zedScale.z *= -1;
-      transform.localScale = zedScale;
+              facingRight          = !facingRight;
+      Vector3 zedScale             =  transform.localScale;
+              zedScale.z           = -zedScale.z;
+              transform.localScale =  zedScale;
    }
 }

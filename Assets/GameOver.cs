abad6327e96e111aef44cb8e0bb611fbea3ec988
Jared DeRemer <jaredderemer@yahoo.com>
2017-03-29 10:36:38 -0500

    Game Over panel finished

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -15,14 +15,30 @@ public class GameOver : MonoBehaviour {
    //public Text statsText;
    private Text statsText;
    
-   private string firstName;
-   private string lastName;
+   private string firstName; // User's entered first name
+   private string lastName;  // User's entered last name
+   
+   public InputField firstNameInput;
+   public InputField lastNameInput;
+   
+   public GameObject afterSubmissionText; // Message to be displayed after player submits score
    
    void Awake()
    {
       //Get a component reference to ShowPanels attached to this object, store in showPanels variable
-		showPanels = GetComponent<ShowPanels> ();
+		showPanels  = GetComponent<ShowPanels> ();
       pauseScript = GetComponent<Pause> ();
+   }
+   
+   void Update ()
+   {
+      // Check if tab is pressed
+      if(Input.GetKeyDown (KeyCode.Tab) && firstNameInput.GetComponent<InputField>().isFocused)
+      {
+         // Place cursor in lastName input fields
+         lastNameInput.Select();
+         lastNameInput.ActivateInputField();
+      }
    }
    
    public void endGame()
@@ -39,6 +55,10 @@ public class GameOver : MonoBehaviour {
       // Set player statistics text
       statsText = GameObject.Find("valuesText").GetComponent<Text> ();
       statsText.text = "30:88\n324\n5"; // playerTime.ToString() + "\n" + monkeys.ToString() + "\n" + easterEggs.ToString;
+      
+      // Place cursor in firstName input fields
+      firstNameInput.Select();
+      firstNameInput.ActivateInputField();
    }
    
    public void submitScore()
@@ -47,6 +67,13 @@ public class GameOver : MonoBehaviour {
       firstName = GameObject.Find("firstNameText").GetComponent<Text>().text;
       lastName = GameObject.Find("lastNameText").GetComponent<Text>().text;
       
-      System.IO.File.AppendAllText("\\\\csweb\\Classes\\SEI\\Castaway\\adventure.txt", firstName + " " + lastName + " 999 99 9 9999999");
+      System.IO.File.AppendAllText("\\\\csweb\\Classes\\SEI\\Castaway\\adventure.txt", firstName + " " + lastName + " 999 99 9 9999999\n");
+      
+      // Hide submission fields and button
+      GameObject.Find("EnterName").SetActive(false);
+      GameObject.Find("SubmitScore").SetActive(false);
+      
+      // Show message notifying the player the score was submitted
+      afterSubmissionText.SetActive(true);
    }
 }

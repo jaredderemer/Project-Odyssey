bd7a052a6ffe3a4b6c1ae588ff1b28e574c16f6a
Will Reaves <wjreaves93@yahoo.com>
2017-03-29 16:43:44 -0500

    initial AI scripting

---
+++ Assets/CharacterScenes/Scripts/MonkeyDetection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyDetection : MonoBehaviour
+{
+	private bool playerInView = false;
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		if (playerInView)
+		{
+			followPlayer ();
+		}
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerInView = true;
+		}
+	}
+
+	void onTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerInView = false;
+		}
+	}
+
+	void followPlayer()
+	{
+
+	}
+}

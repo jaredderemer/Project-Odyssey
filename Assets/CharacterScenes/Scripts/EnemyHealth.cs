23be5afcb3cf5792fad0b8808fadad37cee5e129
Casey Fenton <casey.fenton@outlook.com>
2017-03-31 08:58:36 -0500

    Enemy Health and Health Bar

---
+++ Assets/CharacterScenes/Scripts/EnemyHealth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+   public float enemyMaxHealth;
+   public float damageModifier;
+   //public GameObject damageParticles;
+   //public bool drops;
+   //public GameObject drop;
+
+   float currentHealth;
+   public Slider enemyHealthIndicator;
+
+	// Use this for initialization
+	void Start ()
+   {
+      currentHealth = enemyMaxHealth;
+      enemyHealthIndicator.maxValue = enemyMaxHealth;
+      enemyHealthIndicator.value = currentHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+   {
+		
+	}
+
+   public void AddDamage(float damage)
+   {
+      enemyHealthIndicator.gameObject.SetActive(true);
+      damage = damage * damageModifier;
+      if (damage <= 0f)
+      {
+         return;
+      }
+      currentHealth -= damage;
+      enemyHealthIndicator.value = currentHealth;
+
+      if (currentHealth <= 0)
+      {
+         MakeDead();
+      }
+   }
+
+   void MakeDead()
+   {
+      Destroy(gameObject.transform.root.gameObject);
+      //if (drops)
+      //{
+      //   Instantiate(drop, transform.position, transform.rotation);
+      //}
+   }
+}

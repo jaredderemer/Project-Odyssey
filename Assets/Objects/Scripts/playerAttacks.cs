38418829a1ffd810d76b29979f9acf0d706f31d8
NickTheRedFox <NickTheRedFox@gmail.com>
2017-04-01 17:27:51 -0500

    Coconuts have velocity and hurt...

--- Assets/Objects/Scripts/playerAttacks.cs
+++ Assets/Objects/Scripts/playerAttacks.cs
@@ -2,35 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using System;   // DELETE
-using System.IO;// DELETE
-
 public class playerAttacks : MonoBehaviour 
 {
+
     public int hasStick;
     public Rigidbody coconut;
     public Transform FireTransform;
     private string Fire_Button;
-    public int x;  /// coconut velocity
-    public int y;  /// coconut velocity
-
-    
+    public int x;
+    public int y;
 
 	// Use this for initialization
 	void Start () 
     {
-        var fileName = "Nicks Debugger.txt";
-
-        if (File.Exists(fileName))
-        {
-            Debug.Log(fileName + " already exists.");
-            return;
-        }
-
-         var sr = File.CreateText(fileName);
-        sr.WriteLine("This Is to test output script, simple but effective. \n");
-        //sr.Close();
+		
 	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    
 
     
    void meleeAttack()
@@ -46,12 +40,8 @@ public class playerAttacks : MonoBehaviour
    // Creates a coconut infront of player
    public void rangeAttack()
    {
-       Rigidbody coconutInstance = Instantiate(coconut, FireTransform.localPosition , Quaternion.identity) as Rigidbody;
-       coconutInstance.position = (FireTransform.position + gameObject.transform.position);
+       Rigidbody coconutInstance = Instantiate(coconut, FireTransform.position + gameObject.transform.position, FireTransform.rotation) as Rigidbody;
        coconutInstance.velocity = new Vector3(x, y, 0);
-
-       print("This Is the transform object position: " + coconutInstance.position);
        Debug.Log(FireTransform.position + gameObject.transform.position);
-       //Instantiate(coconutInstance);
    }
 }

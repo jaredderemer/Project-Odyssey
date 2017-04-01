8787dbd1bcd5f77ab3ec3dce972421ac857e82a7
NickTheRedFox <NickTheRedFox@gmail.com>
2017-04-01 14:23:23 -0500

    Updated

--- Assets/Objects/Scripts/playerAttacks.cs
+++ Assets/Objects/Scripts/playerAttacks.cs
@@ -7,11 +7,12 @@ using System.IO;// DELETE
 
 public class playerAttacks : MonoBehaviour 
 {
-
     public int hasStick;
     public Rigidbody coconut;
     public Transform FireTransform;
     private string Fire_Button;
+    public int x;  /// coconut velocity
+    public int y;  /// coconut velocity
 
     
 
@@ -26,9 +27,9 @@ public class playerAttacks : MonoBehaviour
             return;
         }
 
-        var sr = File.CreateText(fileName);
-        sr.WriteLine("This Is to test the Values of the Inventory. \n");
-        sr.Close();
+         var sr = File.CreateText(fileName);
+        sr.WriteLine("This Is to test output script, simple but effective. \n");
+        //sr.Close();
 	}
 
     
@@ -45,7 +46,11 @@ public class playerAttacks : MonoBehaviour
    // Creates a coconut infront of player
    public void rangeAttack()
    {
-       Rigidbody coconutInstance = Instantiate(coconut, FireTransform.position + gameObject.transform.position, FireTransform.rotation) as Rigidbody;
+       Rigidbody coconutInstance = Instantiate(coconut, FireTransform.localPosition , Quaternion.identity) as Rigidbody;
+       coconutInstance.position = (FireTransform.position + gameObject.transform.position);
+       coconutInstance.velocity = new Vector3(x, y, 0);
+
+       print("This Is the transform object position: " + coconutInstance.position);
        Debug.Log(FireTransform.position + gameObject.transform.position);
        //Instantiate(coconutInstance);
    }

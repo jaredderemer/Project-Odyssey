ae04c1b75b028252b377c19b5456d4d509e11eb9
Juju Moong <Juju@juju-air.local>
2017-04-02 22:40:31 -0500

    switching character - testing

---
+++ Assets/CharacterSelection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour {
+
+   private GameObject[] characterList;
+   private int index;
+	// Use this for initialization
+	void Start () 
+   {
+      index = PlayerPrefs.GetInt ("CharacterSelected");
+      characterList = new GameObject[transform.childCount];
+
+      Debug.Log (transform.childCount);
+
+      // Fill array with models
+      for (int i = 0; i < transform.childCount; i++) 
+      {
+         characterList [i] = transform.GetChild (i).gameObject;
+      }
+
+      // Deactivate gameobject
+      foreach (GameObject go in characterList) 
+      {
+         go.SetActive (false);
+      }
+
+      // Activate selected index
+      if (characterList [index]) 
+      {
+         characterList [index].SetActive (true);
+      } 
+	}
+	
+   public void selectCharacter()
+   {
+      // Toggle off current model
+      characterList[index].SetActive(false);
+
+      index++;
+      if (index == characterList.Length) 
+      {
+         index = 0;
+      }
+
+      // Toggle on new model
+      characterList[index].SetActive(true);
+
+   }
+	
+   public void Confirm()
+   {
+      PlayerPrefs.SetInt ("CharacterSelected", index);
+      SceneManager.LoadScene ("scene2");
+   }
+}
---
+++ Assets/Environment/Scripts/switchObjectTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class switchObjectTest : MonoBehaviour {
+
+   public GameObject list;
+
+   void OnTriggerStay (Collider col)
+   {
+      if (col.tag == "Player" && Input.GetKey(KeyCode.E)) {
+         list.GetComponent<CharacterSelection> ().selectCharacter ();
+
+      }
+      if (col.tag == "Player" && Input.GetKey (KeyCode.C)) {
+         list.GetComponent<CharacterSelection> ().Confirm ();
+      }
+   }
+}

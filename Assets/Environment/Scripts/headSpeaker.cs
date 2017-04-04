a4779edfbaf838a587bdcca9a465b8c82760ada8
Jonathan Rigsby <jonathanrigsby@gmail.com>
2017-04-04 15:05:26 -0500

    light mapping the right scene 4 and added some puns to for the easter head

--- Assets/Environment/Scripts/headSpeaker.cs
+++ Assets/Environment/Scripts/headSpeaker.cs
@@ -15,11 +15,18 @@ public class headSpeaker : MonoBehaviour {
       isUsed = false;
 
       // Populating array of easter puns
-      messageList[0] = "Message 1";
-      messageList[1] = "Message 2";
-      messageList[2] = "Message 3";
-      messageList[3] = "Message 4";
-      messageList[4] = "Message 5";
+      messageList[0] = "Don't spell part backwards. It's a trap.";
+      messageList[1] = "I relish the fact that you've mustard the strength to ketchup to me.";
+      messageList[2] = "I wanna make a joke about Sodium...Na...";
+      messageList[3] = "I found a rock yesterday which measured 1,760 yards in length. Must be some kind of milestone.";
+      messageList[4] = "It's hard to explain puns to kleptomaniacs because they always take things literally";
+      messageList[5] = "Do you know why I make puns? Because it's my respunsibility.";
+      messageList[6] = "Message 7";
+      messageList[7] = "Message 8";
+      messageList[8] = "Message 9";
+      messageList[9] = "Message 10";
+      messageList[10] = "Message 11";
+      messageList[11] = "Message 12";
 	}
 	
 	// Update is called once per frame

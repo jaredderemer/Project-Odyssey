51ca345766f749679d52b42a9890c1f5e3ca46eb
NickTheRedFox <NickTheRedFox@gmail.com>
2017-04-05 21:02:32 -0500

    Staging: Adding a movie folder and movies are working...

---
+++ Assets/Movie/movieController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movieController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+       // this line of code will make the Movie Texture begin playing
+       ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

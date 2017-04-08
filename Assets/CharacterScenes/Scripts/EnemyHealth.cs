0455cec81c69ed2334dbd273a7071c66dd7caea9
Jonathan Rigsby <124256@studentnet.int>
2017-04-07 21:48:15 -0500

    worked on character animations

--- Assets/CharacterScenes/Scripts/EnemyHealth.cs
+++ Assets/CharacterScenes/Scripts/EnemyHealth.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -53,4 +53,4 @@ public class EnemyHealth : MonoBehaviour
       //   Instantiate(drop, transform.position, transform.rotation);
       //}
    }
-}
+} */

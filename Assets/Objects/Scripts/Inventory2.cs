97cf863cab8f4e9c9caa4cbb3831b3f2f4493b45
NickTheRedFox <NickTheRedFox@gmail.com>
2017-04-19 12:40:58 -0500

    Master: removed quantity as a factor for inventory items---Its not being used at all

--- Assets/Objects/Scripts/Inventory2.cs
+++ Assets/Objects/Scripts/Inventory2.cs
@@ -105,28 +105,15 @@ public class Inventory2 : MonoBehaviour
       Debug.Log("slot 3: " + inventory[2].itemID + ", " + inventory[2].quantity);
       Debug.Log("slot 4: " + inventory[3].itemID + ", " + inventory[3].quantity);
       Debug.Log("--------------------------");
-      
-      bool test1;
-      int test2;
-      
+  
       for (int i = 0; i < numItemSlots; i++)
       {
          // Looks for item in the inventory and consumes it
          if (inventory[i].itemID == itemID)
          {
-            // Check for single item in inventory
-            if (inventory[i].quantity == 1)
-            {
                // Empty the inventory slot and sort list
                sortList(i);
                return 1;
-            }
-            else if(inventory[i].quantity > 1)
-            {
-               // Decrement the quantity
-               inventory[i].quantity -= 1;
-               return 1;
-            }
          }
          if (i == (numItemSlots - 1))
          {
@@ -183,6 +170,7 @@ public class Inventory2 : MonoBehaviour
 
 
    // Drops the first item on the inventory
+   // THIS IS FOR TESTING ONLY
    public void dropItem()
    {
        // Player drops an item
